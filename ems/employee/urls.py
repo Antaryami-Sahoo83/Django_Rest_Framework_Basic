87d3faf8aaaaa2cee7aaeef931270b638a75dee5
Antaryami Sahoo <mail2sahooantaryami@gmail.com>
2024-06-29 19:47:12 +0530

    Class based model is implemented

--- ems/employee/urls.py
+++ ems/employee/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path,include
 from employee import views
 
-urlpatterns = [
-	path('employee/',views.employee),
-      path('employee/<int:id>/', views.employee_details),
-]
-
-# from rest_framework import routers
-
-# router = routers.DefaultRouter()
-# router.register('employee', views.EmployeeViewSet)
-	
 # urlpatterns = [
-# 	path('', include(router.urls)),
-#       # path('employee/',views.employee),
-#       # path('employee/<int:id>/', views.employee_details),
+# 	path('employee/',views.employee),
+#       path('employee/<int:id>/', views.employee_details),
 # ]
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('employee', views.EmployeeViewSet)
+	
+urlpatterns = [
+	path('', include(router.urls)),
+      # path('employee/',views.employee),
+      # path('employee/<int:id>/', views.employee_details),
+]
--- ems/employee/views.py
+++ ems/employee/views.py
@@ -8,45 +8,45 @@ from rest_framework.decorators import api_view
 from employee.models import Employee
 from employee.serializer import EmployeeSerializer
 
-# from rest_framework import viewsets
+from rest_framework import viewsets
 
-@api_view(['GET','POST'])
-def employee(request):
-      if request.method == "GET":
-            employees = Employee.objects.all()
-            serializer = EmployeeSerializer(employees, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+# @api_view(['GET','POST'])
+# def employee(request):
+#       if request.method == "GET":
+#             employees = Employee.objects.all()
+#             serializer = EmployeeSerializer(employees, many=True)
+#             return Response(serializer.data, status=status.HTTP_200_OK)
       
-      if request.method == "POST":
-            serializer = EmployeeSerializer(data=request.data)
-            if serializer.is_valid():
-                  serializer.save()
-                  return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#       if request.method == "POST":
+#             serializer = EmployeeSerializer(data=request.data)
+#             if serializer.is_valid():
+#                   serializer.save()
+#                   return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET','PUT','DELETE'])
-def employee_details(request,id):
-	employee = get_object_or_404(Employee, id=id)
+# @api_view(['GET','PUT','DELETE'])
+# def employee_details(request,id):
+# 	employee = get_object_or_404(Employee, id=id)
 	
-	if request.method == "GET":
-		serializer = EmployeeSerializer(employee)
-		return Response(serializer.data, status=status.HTTP_200_OK)
+# 	if request.method == "GET":
+# 		serializer = EmployeeSerializer(employee)
+# 		return Response(serializer.data, status=status.HTTP_200_OK)
 
-	if request.method == "PUT":
-		serializer = EmployeeSerializer(employee, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_200_OK)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 	if request.method == "PUT":
+# 		serializer = EmployeeSerializer(employee, data=request.data)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return Response(serializer.data, status=status.HTTP_200_OK)
+# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	if request.method == "DELETE":
-		employee.delete()
-		return Response({"message": "Employee Delete"}, status=status.HTTP_200_OK)
+# 	if request.method == "DELETE":
+# 		employee.delete()
+# 		return Response({"message": "Employee Delete"}, status=status.HTTP_200_OK)
 
 
 # Class Based View
 
-# class EmployeeViewSet(viewsets.ModelViewSet):
-# 	query = Employee.objects.all()
-# 	serializer_class = EmployeeSerializer
+class EmployeeViewSet(viewsets.ModelViewSet):
+	queryset = Employee.objects.all()
+	serializer_class = EmployeeSerializer
